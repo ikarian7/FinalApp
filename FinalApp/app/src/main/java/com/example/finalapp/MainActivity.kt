804e4b730577d5867f312223a10d6f64d6fbbd33
ikarian7 <scott.marchant7@gmail.com>
2020-05-25 15:40:29 +0200

    made 4th fragment for tabnav

--- FinalApp/app/src/main/java/com/example/finalapp/MainActivity.kt
+++ FinalApp/app/src/main/java/com/example/finalapp/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+                R.id.navigation_character, R.id.navigation_info, R.id.navigation_combat, R.id.navigation_story))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
---
+++ FinalApp/app/src/main/java/com/example/finalapp/ui/story/StoryFragment.kt
@@ -0,0 +1,31 @@
+package com.example.finalapp.ui.story
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.finalapp.R
+
+class StoryFragment : Fragment() {
+
+    private lateinit var storyViewModel: StoryViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        storyViewModel =
+            ViewModelProviders.of(this).get(StoryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        storyViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
