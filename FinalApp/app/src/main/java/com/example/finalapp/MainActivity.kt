cf366a26467faaaead59fab7f1c82b823bf86f12
ikarian7 <scott.marchant7@gmail.com>
2020-05-27 22:05:08 +0200

    add character button works
    when clicked alert pops-up with choices

--- FinalApp/app/src/main/java/com/example/finalapp/MainActivity.kt
+++ FinalApp/app/src/main/java/com/example/finalapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.finalapp
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -10,6 +12,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.add_char_dialogue.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +28,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_character, R.id.navigation_info, R.id.navigation_combat, R.id.navigation_story))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navView.itemIconTintList = null
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -36,7 +40,17 @@ class MainActivity : AppCompatActivity() {
         val id = item.getItemId()
 
         if (id == R.id.action_one) {
-            Toast.makeText(this, "Add character", Toast.LENGTH_LONG).show()
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.add_char_dialogue, null)
+            //AlertDialogBuilder
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("Add Character")
+            //show dialog
+            val  mAlertDialog = mBuilder.show()
+            mDialogView.dialogCancelBtn.setOnClickListener {
+                //dismiss dialog
+                mAlertDialog.dismiss()
+            }
             return true
         }
         if (id == R.id.action_two) {
