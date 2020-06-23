2996a13a28513e3d2be240794860d6291fa72b22
ikarian7 <scott.marchant7@gmail.com>
2020-06-23 21:13:09 +0200

    cartel info added

--- FinalApp/app/src/main/java/com/example/finalapp/ui/info/InfoFragment.kt
+++ FinalApp/app/src/main/java/com/example/finalapp/ui/info/InfoFragment.kt
@@ -1,13 +1,16 @@
 package com.example.finalapp.ui.info
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.finalapp.R
 import com.example.finalapp.ui.ActivityViewModel
+import kotlinx.android.synthetic.main.fragment_info.*
 
 class InfoFragment : Fragment() {
     private lateinit var infoViewModel: ActivityViewModel
@@ -19,5 +22,26 @@ class InfoFragment : Fragment() {
     ): View? {
         infoViewModel = ViewModelProvider(this).get(ActivityViewModel::class.java)
         return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        infoViewModel.currentChara.observe(viewLifecycleOwner, Observer { currentChara ->
+            var cartelinfo = "info"
+            if (currentChara != null) {
+                if(currentChara.cartel == "The Circle")
+                    cartelinfo = this.getString(R.string.CircleInfo)
+                else{
+                    cartelinfo = this.getString(R.string.FamilyInfo)
+                }
+             tvCartelInfo.text = cartelinfo
+            }
+            Log.d("cartelinfo", cartelinfo)
+        })
     }
 }
